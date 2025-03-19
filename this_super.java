b798aeed06b51a916a00b38e18db1a965c7d2904
Moksh <moxhsharma878@gmail.com>
2025-03-20 00:35:16 +0530

    Add files via upload

---
+++ this_super.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.Random;
+    class game{
+       int number;
+       int userinput;
+        int noofguesses = 0 ;
+        public int getnoofguesses(){
+            return noofguesses;
+        }
+        public void setnoofguesses(int noofguesses){
+                this.noofguesses = noofguesses;
+        }
+        game(){
+            
+            Random rand = new Random();
+        this.number =  (rand.nextInt(100));    
+        }
+        void takeuseinput(){
+            System.out.println("guess the number");
+            
+            Scanner sc =  new Scanner(System.in);
+
+userinput = sc.nextInt();
+        }
+    
+      
+        boolean irohit(){
+        noofguesses++;
+             
+        if (userinput==number){
+            System.out.println("congras you huess the right"+ number+ " wow you guessed it in  attemds " + noofguesses );
+            return true;
+        }  
+        else if (userinput <number){
+            System.out.println("to low...........");
+            
+        
+        }
+        else if (userinput >number){
+            System.out.println("to high............");
+      
+               
+
+        }
+                return false;
+    }             
+    }
+public class this_super{
+    public static void main(String[] args) {
+        game g = new game();
+        boolean b = false;
+        while(!b){
+             g.takeuseinput();
+           b =   g.irohit();
+            
+        }
+
+    }
+    }
